d77d5d53475afd07c7b244638d1d4fbe742d5169
menglz <1345057558@qq.com>
2024-11-25 10:35:37 +0800

    feat:本地初始化数据库,新建用户模块数据表,定义用户模块地段

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,85 @@
+/**
+ * 创建用户表
+ */
+
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from './role.entity';
+
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+    comment: '用户名',
+  })
+  username: string;
+
+  @Column({
+    length: 50,
+    comment: '密码',
+  })
+  password: string;
+
+  @Column({
+    name: 'nick_name',
+    length: 50,
+    comment: '昵称',
+  })
+  nickName: string;
+
+  @Column({
+    length: 50,
+    comment: '邮箱',
+  })
+  email: string;
+
+  @Column({
+    length: 100,
+    comment: '头像',
+    nullable: true, // 可以为空
+  })
+  headPic: string;
+
+  @Column({
+    length: 20,
+    comment: '手机号',
+    nullable: true, // 可以为空
+  })
+  phoneNumber: string;
+
+  @Column({
+    comment: '是否冻结',
+    default: false,
+  })
+  isFrozen: boolean;
+
+  @Column({
+    comment: '是否是管理员',
+    default: false,
+  })
+  isAdmin: false;
+
+  @CreateDateColumn()
+  createTime: Date;
+
+  @UpdateDateColumn()
+  updateTime: Date;
+
+  @ManyToMany(() => Role)
+  @JoinTable({
+    name: 'user_role',
+  })
+  roles: Role[];
+}
