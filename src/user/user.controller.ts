170414d5c7776731356bd25df1013e2b39bb9610
menglz <1345057558@qq.com>
2024-11-25 13:37:41 +0800

    feat:注册功能完成

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,16 +6,38 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { RedisService } from '../redis/redis.service';
+import { EmailService } from '../email/email.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private redisService: RedisService,
+    private emailService: EmailService,
+  ) {}
 
+  // 用户注册
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
     return await this.userService.register(registerUser);
+  }
+
+  // 用户注册获取验证码
+  @Get('register-captcha')
+  async captcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 6);
+    await this.redisService.set(`captcha_${address}`, code);
+
+    this.emailService.sendMail({
+      to: address,
+      subject: '注册验证码',
+      html: `<p>你的注册验证码是 ${code}<p>`,
+    });
+    return '发送成功';
   }
 }
